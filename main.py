f765918687dd8c9e12bec25b3a5ab78c1e5126e6
princekumar-cpu <57572355+princekumar-cpu@users.noreply.github.com>
2022-05-25 23:17:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-
+from time import time
+from time import sleep
 words = []
 temp = []
 
@@ -12,6 +13,16 @@ print("\n\n\n\n_______________________________________Wordle Clone______________
 print("You have 6 chances for today\n")
 print("Make sure that the word you choice that have some meaning and word must be of five digit\n\n")
 print("So, let's start the game...........\n")
+
+def countdown(t):
+    mins, sec = divmod(t,60)
+    hour, mins = divmod(mins,60)
+    timer = '{:02d}:{:02d}:{:02d}'.format(hour,mins,sec)          
+    print("Next Wordle : {}".format(timer),end='\r')
+    sleep(1)
+    if t == 0:
+        main()
+    countdown(t-1)
 
 def main():
     count_chance = 0
@@ -30,6 +41,7 @@ def main():
             count_chance += 1
             if ch_word == rand_word:
                 print("Congratulations!  Yon Win this game")
+                countdown(24*60*60)
                 break
             if ch_word in words:
                 for i in ch_word:
@@ -49,7 +61,8 @@ def main():
             print("The word you have chosen have more or less than five character\nDon't worry your is not gone for this")
     
     if flag:
-        print("\n\n__________Sorry__________\nYou lost this game please try it tomorrow\n")
+        print("\n\n__________Sorry__________\nYou lost this game. Please try it tomorrow\n\n")
+        countdown(24*60*60)
 
 if __name__ == '__main__':
     main()
